528f6ad4b7dba5914e4e79f1b620b9e9c5999207
ariaan ahmed <you@example.com>
2023-05-30 10:37:59 +0600

    77-8 Display cart items to review before order

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 app.use(cors());
@@ -55,6 +55,13 @@ async function run() {
 
             const result = await cartsCollection.insertOne(item);
             res.send(result)
+        })
+
+        app.delete('/carts/:id', async(req, res) => {
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)};
+            const result = await cartsCollection.deleteOne(query);
+            res.send(result);
         })
 
         // Send a ping to confirm a successful connection
