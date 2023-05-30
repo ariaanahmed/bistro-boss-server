847569dc8a3d98c1d0191016037c879d0f18007a
ariaan ahmed <you@example.com>
2023-05-30 16:01:31 +0600

    78-3 Save User if he doesnt exists in the database

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 
+// middleware
 app.use(cors());
 app.use(express.json())
 
@@ -25,14 +26,36 @@ async function run() {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
 
+        const usersCollection = client.db("bistroDB").collection("users");
         const menuCollection = client.db("bistroDB").collection("menu");
         const reviewsCollection = client.db("bistroDB").collection("reviews");
         const cartsCollection = client.db("bistroDB").collection("carts");
+        
+        // user related APIs
+        
+        app.get('/users', async(req, res) => {
+            const result = await usersCollection.find().toArray();
+            res.send(result)
+        })
 
+        app.post('/users', async(req, res) => {
+            const user = req.body;
+            console.log(user);
+            const query = {email: user.email};
+            const existingUser = await usersCollection.findOne(query);
+            if(existingUser){
+                return res.send({message: 'user already exists'})
+            }
+            const result = await usersCollection.insertOne(user);
+            res.send(result)
+        })
+
+        // menu related APIs
         app.get('/menu', async(req,  res) => {
             const result = await menuCollection.find().toArray();
             res.send(result)
         })
+        // review related APIs
         app.get('/reviews', async(req,  res) => {
             const result = await reviewsCollection.find().toArray();
             res.send(result)
@@ -51,8 +74,6 @@ async function run() {
         })
         app.post('/carts', async(req, res) => {
             const item = req.body;
-            console.log(item);
-
             const result = await cartsCollection.insertOne(item);
             res.send(result)
         })
