29badd504c21176677d2fd054e75ebb33f231e19
ariaan ahmed <you@example.com>
2023-06-02 23:50:52 +0600

    79-9 Delete menu item with VerifyAdmin middleware

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const jwt = require('jsonwebtoken');
 const port = process.env.PORT || 5000;
 
 
@@ -11,6 +11,21 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json())
 
+const verifyJWT = (req, res, next) => {
+    const authorization = req.headers.authorization;
+    if (!authorization) {
+        return res.status(401).send({ error: true, message: 'unauthorized access' })
+    }
+    // bearer token
+    const token = authorization.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(401).send({ error: true, message: 'unauthorized access' })
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
 
 const uri = `mongodb+srv://${process.env.USER_DB}:${process.env.USER_PASS}@cluster0.jvx2mqj.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -36,19 +51,37 @@ async function run() {
         app.post('/jwt', (req, res) => {
             const user = req.body;
             const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
-            res.send({token})
+            res.send({ token })
         })
 
-        // user related APIs
 
-        app.get('/users', async (req, res) => {
+        // WARNING: use the verifyJWT before using verifyAdmin
+
+        const verifyAdmin = async (req, res, next) => {
+            const email = req.decoded.email;
+            const query = { email: email }
+            const user = await usersCollection.findOne(query);
+            if (user?.role !== 'admin') {
+                return res.status(403).send({ error: true, message: 'forbidden message' })
+            }
+            next();
+        }
+
+
+        /**
+         * 1. do not show secure links to those who should not see the link
+         * 2. use JWT token: verifyJWT
+         * 3. use verifyAdmin middleware
+         */
+
+        // user related APIs
+        app.get('/users', verifyJWT, verifyAdmin, async (req, res) => {
             const result = await usersCollection.find().toArray();
             res.send(result)
         })
 
         app.post('/users', async (req, res) => {
             const user = req.body;
-            console.log(user);
             const query = { email: user.email };
             const existingUser = await usersCollection.findOne(query);
             if (existingUser) {
@@ -56,6 +89,25 @@ async function run() {
             }
             const result = await usersCollection.insertOne(user);
             res.send(result)
+        })
+
+        /* 
+        // security layer:
+        * verifyJWT
+        * email same
+        * check admin
+        *  */
+        app.get('/users/admin/:email', verifyJWT, async (req, res) => {
+            const email = req.params.email;
+
+            if (req.decoded.email !== email) {
+                res.send({ admin: false })
+            }
+
+            const query = { email: email };
+            const user = await usersCollection.findOne(query);
+            const result = { admin: user?.role === 'admin' };
+            res.send(result);
         })
 
         app.patch('/users/admin/:id', async (req, res) => {
@@ -77,6 +129,20 @@ async function run() {
             const result = await menuCollection.find().toArray();
             res.send(result)
         })
+
+        app.post('/menu', verifyJWT, verifyAdmin, async (req, res) => {
+            const newItem = req.body;
+            const result = await menuCollection.insertOne(newItem);
+            res.send(result);
+        })
+
+        app.delete('/menu/:id', verifyJWT, verifyAdmin, async(req, res) => {
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)};
+            const result = await menuCollection.deleteOne(query);
+            res.send(result)
+        })
+
         // review related APIs
         app.get('/reviews', async (req, res) => {
             const result = await reviewsCollection.find().toArray();
@@ -85,11 +151,17 @@ async function run() {
 
         // cart collection APIs
 
-        app.get('/carts', async (req, res) => {
+        app.get('/carts', verifyJWT, async (req, res) => {
             const email = req.query.email;
             if (!email) {
                 res.send([])
             }
+
+            const decodedEmail = req.decoded.email;
+            if (email !== decodedEmail) {
+                return res.status(403).send({ error: true, message: 'forbidden access' })
+            }
+
             const query = { email: email };
             const result = await cartsCollection.find(query).toArray();
             res.send(result);
